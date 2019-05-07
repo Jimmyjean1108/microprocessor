f36a5f698e9d0edde04331593d041dcc5cd1985b
Jimmyjean1108 <30555663+Jimmyjean1108@users.noreply.github.com>
2019-05-07 22:38:41 +0800

    Add files via upload

---
+++ Lab2.1/Debug/src/main.d
@@ -0,0 +1,19 @@
+src/main.o: ../src/main.c \
+ C:/Users/User/workspace/Lab2.1/inc/stm32l476xx.h \
+ C:/Users/User/workspace/Lab2.1/inc/core_cm4.h \
+ C:/Users/User/workspace/Lab2.1/inc/core_cmInstr.h \
+ C:/Users/User/workspace/Lab2.1/inc/cmsis_gcc.h \
+ C:/Users/User/workspace/Lab2.1/inc/core_cmFunc.h \
+ C:/Users/User/workspace/Lab2.1/inc/core_cmSimd.h
+
+C:/Users/User/workspace/Lab2.1/inc/stm32l476xx.h:
+
+C:/Users/User/workspace/Lab2.1/inc/core_cm4.h:
+
+C:/Users/User/workspace/Lab2.1/inc/core_cmInstr.h:
+
+C:/Users/User/workspace/Lab2.1/inc/cmsis_gcc.h:
+
+C:/Users/User/workspace/Lab2.1/inc/core_cmFunc.h:
+
+C:/Users/User/workspace/Lab2.1/inc/core_cmSimd.h:
