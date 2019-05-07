085013cfb7410b3c3bf7c0acd29c90fb26d96574
Jimmyjean1108 <30555663+Jimmyjean1108@users.noreply.github.com>
2019-05-07 22:53:42 +0800

    Add files via upload

---
+++ Lab5.2/Debug/src/main.d
@@ -0,0 +1,37 @@
+src/main.o: ../src/main.c \
+ C:/Users/jimmy/workspace/Lab5.2/inc/stm32l476xx.h \
+ C:/Users/jimmy/workspace/Lab5.2/inc/core_cm4.h \
+ C:/Users/jimmy/workspace/Lab5.2/inc/core_cmInstr.h \
+ C:/Users/jimmy/workspace/Lab5.2/inc/cmsis_gcc.h \
+ C:/Users/jimmy/workspace/Lab5.2/inc/core_cmFunc.h \
+ C:/Users/jimmy/workspace/Lab5.2/inc/core_cmSimd.h \
+ C:/Users/jimmy/workspace/Lab5.2/inc/7seg.h \
+ C:/Users/jimmy/workspace/Lab5.2/inc/stm32l476xx.h \
+ C:/Users/jimmy/workspace/Lab5.2/inc/keypad.h \
+ C:/Users/jimmy/workspace/Lab5.2/inc/helper_functions.h \
+ C:/Users/jimmy/workspace/Lab5.2/inc/led_button.h \
+ C:/Users/jimmy/workspace/Lab5.2/inc/timer.h
+
+C:/Users/jimmy/workspace/Lab5.2/inc/stm32l476xx.h:
+
+C:/Users/jimmy/workspace/Lab5.2/inc/core_cm4.h:
+
+C:/Users/jimmy/workspace/Lab5.2/inc/core_cmInstr.h:
+
+C:/Users/jimmy/workspace/Lab5.2/inc/cmsis_gcc.h:
+
+C:/Users/jimmy/workspace/Lab5.2/inc/core_cmFunc.h:
+
+C:/Users/jimmy/workspace/Lab5.2/inc/core_cmSimd.h:
+
+C:/Users/jimmy/workspace/Lab5.2/inc/7seg.h:
+
+C:/Users/jimmy/workspace/Lab5.2/inc/stm32l476xx.h:
+
+C:/Users/jimmy/workspace/Lab5.2/inc/keypad.h:
+
+C:/Users/jimmy/workspace/Lab5.2/inc/helper_functions.h:
+
+C:/Users/jimmy/workspace/Lab5.2/inc/led_button.h:
+
+C:/Users/jimmy/workspace/Lab5.2/inc/timer.h:
